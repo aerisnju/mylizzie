5d1cca2b17dc689ae4b6bc30956b62eca5563dc8
aerisnju <aerisnju@gmail.com>
2018-03-22 10:01:48 +0800

    Fixed several SGF load problems.

--- src/main/java/wagner/stephanie/lizzie/Lizzie.java
+++ src/main/java/wagner/stephanie/lizzie/Lizzie.java
@@ -3,8 +3,6 @@ package wagner.stephanie.lizzie;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.toomasr.sgf4j.Sgf;
-import com.toomasr.sgf4j.SgfParseException;
-import com.toomasr.sgf4j.board.StoneState;
 import com.toomasr.sgf4j.parser.Game;
 import com.toomasr.sgf4j.parser.GameNode;
 import com.toomasr.sgf4j.parser.Util;
@@ -129,17 +127,7 @@ public class Lizzie {
             Game game = Sgf.createFromPath(gameFilePath);
             GameNode node = game.getRootNode();
 
-            if (game.getProperty("GM") != null && !Objects.equals(game.getProperty("GM"), "1")) {
-                JOptionPane.showMessageDialog(frame, "Error: Not a go game.", "Lizzie", JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-
-            if (game.getProperty("MULTIGOGM") != null && !Objects.equals(game.getProperty("MULTIGOGM"), "1")) {
-                JOptionPane.showMessageDialog(frame, "Error: Not a go game.", "Lizzie", JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-
-            if (game.getProperty("SZ") != null && !Objects.equals(game.getProperty("SZ"), "19")) {
+            if (game.getProperty("SZ") != null && !game.getProperty("SZ").contains("19")) {
                 JOptionPane.showMessageDialog(frame, "Error: Board size is not 19x19.", "Lizzie", JOptionPane.ERROR_MESSAGE);
                 return;
             }
@@ -149,131 +137,145 @@ public class Lizzie {
             clearBoardAndState();
 
             // Process pre-placed stones
-            String prePlacedBlackStoneString = game.getProperty("AB");
-            String prePlacedWhiteStoneString = game.getProperty("AW");
-            List<int[]> prePlacedBlackStones = Collections.emptyList(), prePlacedWhiteStones = Collections.emptyList();
-            if (StringUtils.isNotEmpty(prePlacedBlackStoneString)) {
-                prePlacedBlackStones = Arrays.stream(prePlacedBlackStoneString.split(","))
-                        .map(String::trim)
-                        .map(Util::alphaToCoords)
-                        .collect(Collectors.toList());
-            }
-            if (StringUtils.isNotEmpty(prePlacedWhiteStoneString)) {
-                prePlacedWhiteStones = Arrays.stream(prePlacedWhiteStoneString.split(","))
-                        .map(String::trim)
-                        .map(Util::alphaToCoords)
-                        .collect(Collectors.toList());
-            }
-
-            if (CollectionUtils.isNotEmpty(prePlacedBlackStones) || CollectionUtils.isNotEmpty(prePlacedWhiteStones)) {
-                int maxLength = Math.max(prePlacedBlackStones.size(), prePlacedWhiteStones.size());
-                for (int i = 0; i < maxLength; ++i) {
-                    if (i < prePlacedBlackStones.size()) {
-                        replayer.playMove(true, prePlacedBlackStones.get(i)[0], prePlacedBlackStones.get(i)[1]);
-                    }
-                    if (i < prePlacedWhiteStones.size()) {
-                        replayer.playMove(false, prePlacedWhiteStones.get(i)[0], prePlacedWhiteStones.get(i)[1]);
-                    }
-                }
-            }
+            placePreplacedMove(replayer, game.getProperty("AB"), game.getProperty("AW"));
 
             do {
-                if (node.getMoveNo() < 0 || StringUtils.isEmpty(node.getMoveString())) {
-                    continue;
+                String preplacedBlack = node.getProperty("AB");
+                String preplacedWhite = node.getProperty("AW");
+                if (StringUtils.isNotEmpty(preplacedBlack) || StringUtils.isNotEmpty(preplacedWhite)) {
+                    placePreplacedMove(replayer, preplacedBlack, preplacedWhite);
                 }
-                int[] coords = node.getCoords();
-                if (coords[0] < 19 && coords[0] >= 0 && coords[1] < 19 && coords[1] >= 0) {
-                    replayer.playMove(node.getColorAsEnum().equals(StoneState.BLACK), coords[0], coords[1]);
+                if (node.isMove()) {
+                    if (StringUtils.isNotEmpty(node.getProperty("B"))) {
+                        int[] coords = node.getCoords();
+                        if (coords != null && coords[0] < 19 && coords[0] >= 0 && coords[1] < 19 && coords[1] >= 0) {
+                            replayer.playMove(true, coords[0], coords[1]);
+                        }
+                    }
+                    if (StringUtils.isNotEmpty(node.getProperty("W"))) {
+                        int[] coords = node.getCoords();
+                        if (coords != null && coords[0] < 19 && coords[0] >= 0 && coords[1] < 19 && coords[1] >= 0) {
+                            replayer.playMove(false, coords[0], coords[1]);
+                        }
+                    }
                 }
             }
             while ((node = node.getNextNode()) != null);
-        } catch (SgfParseException e) {
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(frame, "Error: cannot load sgf: " + e.getMessage(), "Lizzie", JOptionPane.ERROR_MESSAGE);
         }
     }
 
-    public static void storeGameByPrompting() {
-        Game game = new Game();
+    private static void placePreplacedMove(MoveReplayer replayer, String preplacedBlackStoneString, String preplacedWhiteStoneString) {
+        List<int[]> preplacedBlackStones = Collections.emptyList(), preplacedWhiteStones = Collections.emptyList();
+        if (StringUtils.isNotEmpty(preplacedBlackStoneString)) {
+            preplacedBlackStones = Arrays.stream(preplacedBlackStoneString.split(","))
+                    .map(String::trim)
+                    .map(Util::alphaToCoords)
+                    .collect(Collectors.toList());
+        }
+        if (StringUtils.isNotEmpty(preplacedWhiteStoneString)) {
+            preplacedWhiteStones = Arrays.stream(preplacedWhiteStoneString.split(","))
+                    .map(String::trim)
+                    .map(Util::alphaToCoords)
+                    .collect(Collectors.toList());
+        }
 
-        game.addProperty("FF", "4"); // SGF version: 4
-        game.addProperty("KM", "7.5"); // Lz only support fixed komi
-        game.addProperty("GM", "1"); // Go game
-        game.addProperty("SZ", "19");
-        game.addProperty("CA", "UTF-8");
-        game.addProperty("AP", "MyLizzie");
-
-        BoardHistoryList historyList = board.getHistory();
-        BoardHistoryNode initialNode = historyList.getInitialNode();
-
-        GameNode previousNode = null;
-        for (BoardHistoryNode p = initialNode.next(); p != null; p = p.next()) {
-            GameNode gameNode = new GameNode(previousNode);
-
-            if (previousNode == null) {
-                game.setRootNode(gameNode);
+        if (CollectionUtils.isNotEmpty(preplacedBlackStones) || CollectionUtils.isNotEmpty(preplacedWhiteStones)) {
+            int maxLength = Math.max(preplacedBlackStones.size(), preplacedWhiteStones.size());
+            for (int i = 0; i < maxLength; ++i) {
+                if (i < preplacedBlackStones.size()) {
+                    replayer.playMove(true, preplacedBlackStones.get(i)[0], preplacedBlackStones.get(i)[1]);
+                }
+                if (i < preplacedWhiteStones.size()) {
+                    replayer.playMove(false, preplacedWhiteStones.get(i)[0], preplacedWhiteStones.get(i)[1]);
+                }
             }
+        }
+    }
 
-            // Move node
-            if (Objects.equals(p.getData().lastMoveColor, Stone.BLACK) || Objects.equals(p.getData().lastMoveColor, Stone.WHITE)) {
-                int x, y;
+    public static void storeGameByPrompting() {
+        try {
+            Game game = new Game();
 
-                if (p.getData().lastMove == null) {
-                    // Pass
-                    x = 19;
-                    y = 19;
-                } else {
-                    x = p.getData().lastMove[0];
-                    y = p.getData().lastMove[1];
+            game.addProperty("FF", "4"); // SGF version: 4
+            game.addProperty("KM", "7.5"); // Lz only support fixed komi
+            game.addProperty("GM", "1"); // Go game
+            game.addProperty("SZ", "19");
+            game.addProperty("CA", "UTF-8");
+            game.addProperty("AP", "MyLizzie");
 
-                    if (x < 0 || x >= 19 || y < 0 || y >= 19) {
+            BoardHistoryList historyList = board.getHistory();
+            BoardHistoryNode initialNode = historyList.getInitialNode();
+
+            GameNode previousNode = null;
+            for (BoardHistoryNode p = initialNode.next(); p != null; p = p.next()) {
+                GameNode gameNode = new GameNode(previousNode);
+
+                if (previousNode == null) {
+                    game.setRootNode(gameNode);
+                }
+
+                // Move node
+                if (Objects.equals(p.getData().lastMoveColor, Stone.BLACK) || Objects.equals(p.getData().lastMoveColor, Stone.WHITE)) {
+                    int x, y;
+
+                    if (p.getData().lastMove == null) {
+                        // Pass
                         x = 19;
                         y = 19;
+                    } else {
+                        x = p.getData().lastMove[0];
+                        y = p.getData().lastMove[1];
+
+                        if (x < 0 || x >= 19 || y < 0 || y >= 19) {
+                            x = 19;
+                            y = 19;
+                        }
+                    }
+
+                    String moveKey = Objects.equals(p.getData().lastMoveColor, Stone.BLACK) ? "B" : "W";
+                    String moveValue = Util.coordToAlpha.get(x) + Util.coordToAlpha.get(y);
+
+                    gameNode.addProperty(moveKey, moveValue);
+                }
+
+                if (p.getData().moveNumber > 0) {
+                    gameNode.setMoveNo(p.getData().moveNumber);
+                }
+
+                if (previousNode != null) {
+                    previousNode.addChild(gameNode);
+                }
+
+                previousNode = gameNode;
+            }
+
+            //game.postProcess();
+
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("*.sgf", "SGF");
+            JFileChooser chooser = new JFileChooser(optionSetting.getLastChooserLocation());
+            chooser.setFileFilter(filter);
+            chooser.setMultiSelectionEnabled(false);
+            int result = chooser.showSaveDialog(frame);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                File file = chooser.getSelectedFile();
+                optionSetting.setLastChooserLocation(file.getParent());
+
+                if (!file.getPath().toLowerCase().endsWith(".sgf")) {
+                    file = new File(file.getPath() + ".sgf");
+                }
+                if (file.exists()) {
+                    int ret = JOptionPane.showConfirmDialog(frame, "The SGF file is exists, do you want replace it?", "Warning", JOptionPane.OK_CANCEL_OPTION);
+                    if (ret == JOptionPane.CANCEL_OPTION) {
+                        return;
                     }
                 }
 
-                String moveKey = Objects.equals(p.getData().lastMoveColor, Stone.BLACK) ? "B" : "W";
-                String moveValue = Util.coordToAlpha.get(x) + Util.coordToAlpha.get(y);
-
-                gameNode.addProperty(moveKey, moveValue);
-            }
-
-            if (p.getData().moveNumber > 0) {
-                gameNode.setMoveNo(p.getData().moveNumber);
-            }
-
-            if (previousNode != null) {
-                previousNode.addChild(gameNode);
-            }
-
-            previousNode = gameNode;
-        }
-
-        //game.postProcess();
-
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("*.sgf", "SGF");
-        JFileChooser chooser = new JFileChooser(optionSetting.getLastChooserLocation());
-        chooser.setFileFilter(filter);
-        chooser.setMultiSelectionEnabled(false);
-        int result = chooser.showSaveDialog(frame);
-        if (result == JFileChooser.APPROVE_OPTION) {
-            File file = chooser.getSelectedFile();
-            optionSetting.setLastChooserLocation(file.getParent());
-
-            if (!file.getPath().toLowerCase().endsWith(".sgf")) {
-                file = new File(file.getPath() + ".sgf");
-            }
-            if (file.exists()) {
-                int ret = JOptionPane.showConfirmDialog(frame, "The SGF file is exists, do you want replace it?", "Warning", JOptionPane.OK_CANCEL_OPTION);
-                if (ret == JOptionPane.CANCEL_OPTION) {
-                    return;
-                }
-            }
-
-            try {
                 Sgf.writeToFile(game, file.toPath());
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(frame, "Error: cannot save sgf: " + e.getMessage(), "Lizzie", JOptionPane.ERROR_MESSAGE);
             }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(frame, "Error: cannot save sgf: " + e.getMessage(), "Lizzie", JOptionPane.ERROR_MESSAGE);
         }
     }
 
